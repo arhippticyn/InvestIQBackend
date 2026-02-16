077e765f258dcc653263091571983eaa5a2f894b
arhippticyn <kakashihatake6hokagegygg@gmail.com>
2026-02-17 00:53:42 +0200

    Write connect db to backend & write table user

--- app/models/user.py
+++ app/models/user.py
@@ -1,1 +1,13 @@
 from sqlalchemy.orm import Mapped, mapped_column
+from app.db.base import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column()
+    email: Mapped[str] = mapped_column()
+    hash_password: Mapped[str | None] = mapped_column(nullable=True)
+    provider: Mapped[str] = mapped_column()
+    provider_id: Mapped[str] = mapped_column()
