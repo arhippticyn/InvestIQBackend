e08228a1f42d9b656b3338366b1189cefd4e0d7c
arhippticyn <kakashihatake6hokagegygg@gmail.com>
2026-02-25 00:46:12 +0200

    fix bag in deploy #4

--- app/db/sessions.py
+++ app/db/sessions.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from app.core.config import DB_URL
 
-engine = create_async_engine(DB_URL, echo=True, connect_args={"ssl": True})
+engine = create_async_engine(DB_URL, echo=True)
 
 SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
